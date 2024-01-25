760aa0781c668ee99c728053ac18162f6d9b868a
ikopoki <131412963+ikopoki@users.noreply.github.com>
2024-01-25 19:33:41 +0500

    fixed pagination

--- js/script.js
+++ js/script.js
@@ -1,12 +1,59 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const resizableSwiper = (
+    breakpoint,
+    swiperClass,
+    swiperSettings,
+    callback
+  ) => {
+    let swiper;
 
-const swiper = new Swiper('.swiper', {  
-  direction: 'horizontal',
-  loop: true,
-  
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function (className, settings) {
+      swiper = new Swiper(className, settings);
+
+      if (callback) {
+        callback(swiper);
+      }
+    };
+
+    const checker = function () {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
+      }
+    };
+
+    breakpoint.addEventListener("change", checker);
+    checker();
+  };
+
+  resizableSwiper("(max-width: 767px)", ".swiper", {
+    loop: false,
+    spaceBetween: 16,
+    slidesPerView: 1.3,
     pagination: {
-      el: '.swiper-pagination',
-    }
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      340: {
+        slidesPerView: 1.3,
+      },
+      360: {
+        slidesPerView: 1.5 
+        },
+      450:{ slidesPerView: 2,},
+      550:{ slidesPerView: 2.3,},
+      570: { slidesPerView: 2 },
+      650:{ slidesPerView: 2.8,},
+      700:{ slidesPerView: 4,}
+    },
+  });
 });
+
 const buttonMore = document.querySelector(".more");
 
 let resizeWrapper = function(){
